9c639c6d0a83d01074c30a7c57475abae020c9c8
klucas <lucas.kocheida@ynov.com>
2026-03-04 10:46:10 +0100

    feat: finalisation du système de déconnexion et redirection

--- admin.php
+++ admin.php
@@ -4,7 +4,7 @@ require 'db.php';
 
 // --- SÉCURITÉ : VÉRIFICATION DU RÔLE ---
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
-    header('Location: index.php');
+    header('Location: admin.php');
     exit();
 }
 
--- logout.php
+++ logout.php
@@ -1,1 +1,11 @@
 <?php
+// logout.php
+session_start();
+
+// On détruit toutes les données de session
+$_SESSION = array();
+session_destroy();
+
+// On redirige vers la page de login (Authentification)
+header('Location: login.php');
+exit();
